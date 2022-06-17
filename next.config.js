6d1a6339d1e8a462650fa9b118623e11db838c14
Florian LEROUX <10078837+floriaaan@users.noreply.github.com>
2022-06-17 15:51:18 +0200

    feat(pwa): init next-pwa package

--- next.config.js
+++ next.config.js
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+};
 
-module.exports = nextConfig
+const withPWA = require("next-pwa");
+
+module.exports = withPWA({
+  pwa: {
+    dest: "public",
+    disable: process.env.NODE_ENV === "development",
+  },
+  ...nextConfig,
+});
