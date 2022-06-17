b89b8193c6ccefd478c1fdaf974b8a78c83d45ea
Florian LEROUX <10078837+floriaaan@users.noreply.github.com>
2022-06-17 09:36:27 +0200

    e2e: add devices

--- playwright.config.ts
+++ playwright.config.ts
@@ -9,22 +9,49 @@ const config: PlaywrightTestConfig = {
   webServer: {
     command: "npm run dev",
     port: 3000,
-    timeout: 120 * 1000,
+    timeout: 30 * 1000,
     reuseExistingServer: !process.env.CI,
     ignoreHTTPSErrors: true,
   },
+
+  workers: !process.env.CI ? 5 : undefined,
+  retries: !process.env.CI ? 1 : undefined,
 
   use: {
     trace: "retry-with-trace",
   },
 
-  //   projects: [
-  //     {
-  //       name: "Desktop Edge",
-  //       use: {
-  //         ...devices["Desktop Edge"],
-  //       },
-  //     },
-  //   ],
+  projects: [
+    {
+      name: "Desktop Chrome",
+      use: {
+        ...devices["Desktop Chrome"],
+      },
+    },
+    {
+      name: "iPhone 13 Pro",
+      use: {
+        ...devices["iPhone 13 Pro"],
+      },
+    },
+    {
+      name: "iPhone 13 Mini",
+      use: {
+        ...devices["iPhone 13 Mini"],
+      },
+    },
+    {
+      name: "Pixel 5",
+      use: {
+        ...devices["Pixel 5"],
+      },
+    },
+    {
+      name: "Galaxy S9+",
+      use: {
+        ...devices["Galaxy S9+"],
+      },
+    },
+  ],
 };
 export default config;
