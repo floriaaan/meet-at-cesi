52348041e46cc4cffd82a7ce4c83c317fb83bb81
Florian LEROUX <10078837+floriaaan@users.noreply.github.com>
2022-06-17 15:38:35 +0200

    feat(ui): add ui components

--- next.config.js
+++ next.config.js
@@ -1,8 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    domains: ["i.pravatar.cc"],
+  },
 };
-
 const withPWA = require("next-pwa");
 
 module.exports = withPWA({
