5cf15ef8c9901181ce2378bba4e9da731233932d
Florian LEROUX <10078837+floriaaan@users.noreply.github.com>
2022-11-16 13:56:45 +0100

    ref(pwa): move to separate file and add cookie to dismiss popup

--- components/Layout/Navbar/MobileMenu.tsx
+++ components/Layout/Navbar/MobileMenu.tsx
@@ -8,6 +8,7 @@ import { Category } from "@/components/UI/Link/Category";
 import { HeroTitle } from "@/components/UI/HeroTitle";
 import { Avatar } from "@/components/UI/Avatar";
 import { UserMinimum } from "@/types/User";
+import { PWAPopup } from "@/components/Helpers/PWAPopup";
 
 export const MobileMenu = () => {
   const [isMenuOpen, setIsMenuOpen] = useState<boolean>(false);
@@ -165,64 +166,7 @@ const MobileMenuPanel = ({ isMenuRendered }: { isMenuRendered: boolean }) => {
           </li>
         ))}
       </ul>
-      <CallToInstallProgressiveWebApp isMenuRendered={isMenuRendered} />
+      <PWAPopup isMenuRendered={isMenuRendered} />
     </>
-  );
-};
-
-const CallToInstallProgressiveWebApp = ({
-  isMenuRendered,
-}: {
-  isMenuRendered: boolean;
-}) => {
-  // const [isInstallable, setIsInstallable] = useState<boolean>(false);
-
-  // useEffect(() => {
-  //   // check if the browser supports the beforeinstallprompt event
-  //   if (window.matchMedia("(display-mode: standalone)").matches) {
-  //     setIsInstallable(false);
-  //   } else {
-  //     window.addEventListener("beforeinstallprompt", (e) => {
-  //       e.preventDefault();
-  //       setIsInstallable(true);
-  //     });
-  //   }
-  // }, []);
-
-  const [dismissed, setDismissed] = useState<boolean>(false);
-
-  return (
-    <div
-      className={classNames(
-        "fixed bottom-7 left-0 w-full z-[9999] px-7 transition-opacity duration-300 ease-linear",
-        isMenuRendered && !dismissed ? "opacity-100" : "opacity-0"
-      )}
-      style={{ transitionDelay: `${150 + 25 * 5}ms` }}
-    >
-      <div className="flex flex-col p-3 text-white bg-black gap-y-2">
-        <div className="inline-flex justify-between w-full">
-          <h3 className="text-xl font-bold">
-            Installer {process.env.NEXT_PUBLIC_APP_NAME}
-          </h3>
-          <button onClick={() => setDismissed(true)}>
-            <MdClose className="w-6 h-6" />
-          </button>
-        </div>
-        <p className="text-xs">
-          Pour accéder à {process.env.NEXT_PUBLIC_APP_NAME} plus rapidement,
-          installez-le sur votre appareil.
-        </p>
-        <div className="flex flex-row justify-between w-full mt-2 gap-x-2">
-          <button className="px-2 text-xs">Ne plus me demander</button>
-          <button
-            disabled
-            className="flex items-center justify-center px-4 py-2 text-xs btn__colors"
-          >
-            <MdDownload className="w-4 h-4" />
-            <span className="ml-2">Installer (bientôt)</span>
-          </button>
-        </div>
-      </div>
-    </div>
   );
 };
