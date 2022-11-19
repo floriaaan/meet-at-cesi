319e26d81e66b50d4c4a65ece6f39ed61f97ce5c
Florian LEROUX <10078837+floriaaan@users.noreply.github.com>
2022-11-19 18:12:29 +0100

    fix(preferencesPopup): add a condition to display or not the popup

--- components/Helpers/PreferencesPopup.tsx
+++ components/Helpers/PreferencesPopup.tsx
@@ -5,7 +5,7 @@ import classNames from "classnames";
 
 import { PreferencesForm } from "@/components/Profile/PreferencesForm";
 import { editPreferences, getPreferences } from "@/lib/fetchers";
-// import useDelayedRender from "@/hooks/useDelayedRender";
+import { MdClose } from "react-icons/md";
 
 export const PreferencesPopup = () => {
   const [cookie, setCookie] = useCookies([
@@ -40,10 +40,6 @@ export const PreferencesPopup = () => {
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
-  useEffect(() => {
-    setDismissed(cookieDismissed === "true" || false);
-  }, [cookieDismissed]);
-
   const dismiss = () => {
     setDismissed(true);
     setCookie("meet-preferences_dismissed", "true", { path: "/" });
@@ -56,11 +52,6 @@ export const PreferencesPopup = () => {
    *
    */
   const show = !(preferences || dismissed);
-  // const { mounted, rendered: isMenuRendered } = useDelayedRender(show, {
-  //   enterDelay: 20,
-  //   exitDelay: 300,
-  // });
-
   useEffect(() => {
     if (show) {
       document.body.classList.add("blocking__popup");
@@ -74,33 +65,52 @@ export const PreferencesPopup = () => {
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [show]);
 
-
   return (
-    <div
-      className={classNames(
-        "fixed top-0 left-0 z-50 flex flex-col justify-between w-screen h-screen p-4 duration-150 bg-white border-black xs:h-fit xs:shadow-xl xs:border xs:ml-4 lg:w-full lg:max-w-md xs:w-72 xs:top-auto xs:left-auto xs:bottom-4 xs:right-4 xs:z-10 pb-[5.5rem] xs:pb-4",
-         show ? "opacity-1" : "opacity-0"
-      )}
-    >
-      <h4 className="mb-2 font-bold">Définition du campus et promotion</h4>
+    show && (
+      <>
+        <div
+          className={
+            "absolute top-0 left-0 z-[42] w-screen h-screen bg-black bg-opacity-40 xs:hidden"
+          }
+          onClick={() => setDismissed(true)}
+        ></div>
+        <div
+          className={classNames(
+            "fixed bottom-0 left-0 z-50 flex flex-col justify-between w-screen p-4 duration-150 bg-white border-black h-fit xs:shadow-xl xs:border xs:ml-4 xs:max-w-sm lg:max-w-lg xs:w-full xs:top-auto xs:left-auto xs:bottom-4 xs:right-4 xs:z-10 pb-[5.5rem] xs:pb-4",
+            show ? "opacity-1" : "opacity-0"
+          )}
+        >
+          <div className="inline-flex items-center justify-between w-full">
+            <h4 className="mb-2 font-bold">
+              Définition du campus et promotion
+            </h4>
+            <button
+              onClick={() => setDismissed(true)}
+              className="hidden p-2 xs:block"
+            >
+              <MdClose />
+            </button>
+          </div>
 
-      <PreferencesForm
-        onSubmit={async ({ campus, promotion, promotionYear }) => {
-          dismiss();
-          return editPreferences({ campus, promotion, promotionYear });
-        }}
-        optionalButton={
-          <button
-            type="button"
-            onClick={dismiss}
-            className="pb-2 text-xs underline underline-offset-2"
-          >
-            Ne plus me demander
-          </button>
-        }
-        submitClassName="w-full btn-black text-sm border-b"
-        labelClassName="text-xs font-bold font-black"
-      />
-    </div>
+          <PreferencesForm
+            onSubmit={async ({ campus, promotion, promotionYear }) => {
+              dismiss();
+              return editPreferences({ campus, promotion, promotionYear });
+            }}
+            optionalButton={
+              <button
+                type="button"
+                onClick={dismiss}
+                className="pb-2 text-xs underline underline-offset-2"
+              >
+                Ne plus me demander
+              </button>
+            }
+            submitClassName="w-full btn-black text-sm border-b"
+            labelClassName="text-xs font-bold font-black"
+          />
+        </div>
+      </>
+    )
   );
 };
