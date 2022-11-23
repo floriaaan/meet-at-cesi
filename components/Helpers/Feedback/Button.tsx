fb0d3337522a9c4652539d125d1ae7789627f655
Florian LEROUX <10078837+floriaaan@users.noreply.github.com>
2022-11-23 22:03:54 +0100

    feat(accessibility): add screen reader label for feedback btn

--- components/Helpers/Feedback/Button.tsx
+++ components/Helpers/Feedback/Button.tsx
@@ -7,13 +7,17 @@ export const FeedbackButton = () => {
   const { isFeedbackOpen, setIsFeedbackOpen } = useFeedback();
   return (
     <button
+      aria-label="Feedback open modal button"
       onClick={() => setIsFeedbackOpen(!isFeedbackOpen)}
       className={classNames(
         "flex items-center justify-center w-12 h-12 ",
-        isFeedbackOpen? "bg-primary text-black": "text-white bg-black active:bg-primary active:text-black hover:text-primary"
+        isFeedbackOpen
+          ? "bg-primary text-black"
+          : "text-white bg-black active:bg-primary active:text-black hover:text-primary"
       )}
     >
       <MdFeedback className="w-6 h-6 " />
+      <span className="sr-only">Envoyer un feedback</span>
     </button>
   );
 };
