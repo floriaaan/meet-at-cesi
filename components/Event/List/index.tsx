70fa7a4ca8a64068078c26d81839fd492d70b5c6
Florian LEROUX <10078837+floriaaan@users.noreply.github.com>
2022-11-23 22:05:59 +0100

    feat(event/list): add no event component

--- components/Event/List/index.tsx
+++ components/Event/List/index.tsx
@@ -10,10 +10,16 @@ type Props = {
 
 export const EventList = ({ events, className }: Props) => {
   return (
-    <div className={classNames("flex flex-col gap-y-2 mt-4 md:gap-y-4", className)}>
-      {events.map((event) => (
-        <EventListItem key={event.id} {...event} />
-      ))}
+    <div
+      className={classNames("flex flex-col gap-y-2 mt-4 md:gap-y-4", className)}
+    >
+      {events.length > 0 ? (
+        events.map((event) => <EventListItem key={event.id} {...event} />)
+      ) : (
+        <NoEvents />
+      )}
     </div>
   );
 };
+
+const NoEvents = () => <p className="text-sm">{"Aucun événement correspondant à ces critères."}</p>;
