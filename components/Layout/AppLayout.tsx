9806510f04bf44d91423ae31f45073afea56420f
Florian LEROUX <10078837+floriaaan@users.noreply.github.com>
2022-11-24 21:56:28 +0100

    fix(overflows): fix feedback btn overflow on buttons on bottom-right and profile sidebar height

--- components/Layout/AppLayout.tsx
+++ components/Layout/AppLayout.tsx
@@ -4,7 +4,7 @@ export const AppLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <div className="flex flex-col h-auto min-h-screen">
       <Navbar />
-      <main className="h-full grow">{children}</main>
+      <main className="h-full mb-20 sm:mb-6 grow">{children}</main>
     </div>
   );
 };
