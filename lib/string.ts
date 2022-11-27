edb8c9680f364213ef1b0f3b1a8baef44c8697d4
Florian LEROUX <10078837+floriaaan@users.noreply.github.com>
2022-11-27 12:46:44 +0100

    fix(avatar): prevent from printing undefined

--- lib/string.ts
+++ lib/string.ts
@@ -1,2 +1,10 @@
 export const getPlural = (count: number, singular: string, plural: string) =>
   `${count > 1 ? plural : singular}`;
+
+export const getInitials = (name: string) => {
+  const initials = name
+    .split(" ")
+    .map((name) => name[0])
+    .join("");
+  return initials;
+};
