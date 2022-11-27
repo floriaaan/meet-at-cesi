a480d055cbc36fa22bd84d9d702e48b5a4dbb172
Florian LEROUX <10078837+floriaaan@users.noreply.github.com>
2022-11-27 12:47:02 +0100

    feat(replies): add replyList component

---
+++ components/Event/Comment/ReplyList.tsx
@@ -0,0 +1,22 @@
+import { ExtendedComment } from "@/types/Event";
+import { CommentListItem } from ".";
+
+type ReplyListProps = {
+  replies: ExtendedComment[];
+  setCommentList: (comments: ExtendedComment[]) => void;
+};
+
+export const ReplyList = ({ replies, setCommentList }: ReplyListProps) => {
+  return (
+    <div className="w-full flex flex-col gap-y-0.5">
+      {replies.map((reply) => (
+        <CommentListItem
+          isReply={true}
+          comment={reply}
+          setCommentList={setCommentList}
+          key={reply.id}
+        />
+      ))}
+    </div>
+  );
+};
