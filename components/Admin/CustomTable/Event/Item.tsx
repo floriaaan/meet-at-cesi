7d814c3d6551ec453be02f2e6a3e744d523c498e
Florian LEROUX <10078837+floriaaan@users.noreply.github.com>
2022-12-07 13:57:31 +0100

    feat(customTable): add custom table component + eventTableItem

---
+++ components/Admin/CustomTable/Event/Item.tsx
@@ -0,0 +1,70 @@
+import { useState } from "react";
+import { useRouter } from "next/router";
+
+import { AvatarWithName } from "@/components/UI/Avatar/WithName";
+import { getPlural } from "@/lib/string";
+import { ExtendedEvent } from "@/types/Event";
+import { EventTableModal } from "@/components/Admin/CustomTable/Event/Modal";
+
+export const EventTableItem = (props: ExtendedEvent) => {
+  const router = useRouter();
+  const { id } = router.query;
+  const [isModalOpen, setIsModalOpen] = useState(id === props.id);
+  const openModal = () => {
+    setIsModalOpen(true);
+    router.push(`/admin/event?id=${props.id}`, undefined, { shallow: true });
+  };
+  const closeModal = () => {
+    setIsModalOpen(false);
+    router.push(`/admin/event`, undefined, { shallow: true });
+  };
+
+  const { title, creator, participants, comments, ...event } = props;
+  return (
+    <>
+      <tr
+        className="text-black bg-white cursor-pointer w-fit hover:bg-neutral-100"
+        onClick={openModal}
+      >
+        <td className="max-w-[16rem] px-6 py-4 truncate">{title}</td>
+        <td>
+          <AvatarWithName
+            user={creator}
+            avatarClassName="w-6 h-6 text-xs"
+            textClassName="text-[0.75rem] font-bold"
+            direction="row"
+          />
+        </td>
+        <td>
+          {participants.length > 0 ? (
+            <span className="inline-flex items-center text-xs text-left text-black gap-x-1">
+              <strong>{participants.length}</strong>
+              {getPlural(participants.length, "participant", "participants")}
+            </span>
+          ) : (
+            <span className="text-xs text-black text-left inline-flex items-center gap-x-0.5">
+              aucun participant
+            </span>
+          )}
+        </td>
+        <td>
+          {comments.length > 0 ? (
+            <span className="inline-flex items-center text-xs text-left text-black gap-x-1">
+              <strong>{comments.length}</strong>
+              {getPlural(comments.length, "commentaire", "commentaires")}
+            </span>
+          ) : (
+            <span className="text-xs text-black text-left inline-flex items-center gap-x-0.5">
+              aucun commentaire
+            </span>
+          )}
+        </td>
+      </tr>
+      <EventTableModal
+        {...props}
+        isModalOpen={isModalOpen}
+        closeModal={closeModal}
+      />
+    </>
+  );
+};
---
+++ components/Admin/CustomTable/index.tsx
@@ -0,0 +1,51 @@
+import classNames from "classnames";
+import dynamic from "next/dynamic";
+import { ReactNode } from "react";
+
+export type CustomTableProps = {
+  title: string;
+  subtitle?: string;
+  items: any[];
+  columns: string[];
+  renderItem: (item: any) => ReactNode;
+};
+
+const CustomTable = ({
+  title,
+  subtitle,
+  columns,
+  items,
+  renderItem,
+}: CustomTableProps) => {
+  return (
+    <div className="overflow-hidden bg-white border border-black border-dashed">
+      <div className="px-4 py-5 sm:px-6">
+        <h3 className="text-lg font-bold leading-6 text-black">{title}</h3>
+        <p className="max-w-2xl mt-1 text-sm text-neutral-500">
+          {subtitle || "Détails et statistiques"}
+        </p>
+      </div>
+      <div className="border-t border-black border-dashed">
+        <table className="w-full overflow-x-scroll">
+          <thead className={classNames("px-4 py-5 bg-neutral-50 md:px-6")}>
+            <tr>
+              {columns.map((column) => (
+                <td
+                  key={column}
+                  className="p-4 text-sm font-medium text-left text-neutral-500"
+                >
+                  {column}
+                </td>
+              ))}
+            </tr>
+          </thead>
+          <tbody>{items.map((item) => renderItem(item))}</tbody>
+        </table>
+      </div>
+    </div>
+  );
+};
+
+export default dynamic(() => Promise.resolve(CustomTable), {
+  ssr: false,
+});
