316036dcceb0942a086d7373fa3cbfa90da855ca
Florian LEROUX <10078837+floriaaan@users.noreply.github.com>
2022-12-09 23:22:39 +0100

    ref(profileLayout): add a prop to hide profile sidebar on [id]

--- components/Layout/Profile/Layout.tsx
+++ components/Layout/Profile/Layout.tsx
@@ -1,11 +1,17 @@
 import { ProfileLayoutSidebar } from "@/components/Layout/Profile/Sidebar";
+import { ReactNode } from "react";
 
-export const ProfileLayout = ({ children }: { children: React.ReactNode }) => {
+type Props = { children: ReactNode; noSidebar?: boolean };
+
+export const ProfileLayout = ({ children, noSidebar = false }: Props) => {
   return (
     <div className="flex w-full min-h-full grow" aria-label="Profile wrapper">
-      <ProfileLayoutSidebar />
-      <div className="flex flex-col w-full h-full overflow-y-auto grow" aria-label="Profile main section">
-      {children}
+      {!noSidebar ? <ProfileLayoutSidebar /> : null}
+      <div
+        className="flex flex-col w-full h-full overflow-y-auto grow"
+        aria-label="Profile main section"
+      >
+        {children}
       </div>
     </div>
   );
