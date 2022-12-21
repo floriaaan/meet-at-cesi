a6d1f3b9c6b4611bd425d2d1e34b560441d33122
Florian LEROUX <10078837+floriaaan@users.noreply.github.com>
2022-12-21 16:29:23 +0100

    ref(event/filter): add chip for selected query params

--- components/Event/Filter/Provider.tsx
+++ components/Event/Filter/Provider.tsx
@@ -1,8 +1,15 @@
-import { createContext, ReactNode, useContext, useState } from "react";
+import {
+	createContext,
+	ReactNode,
+	useContext,
+	useEffect,
+	useState,
+} from "react";
 import {
 	DEFAULT_INITIALS,
 	FilterValues,
 } from "@/components/Event/Filter/Sidebar";
+import { useRouter } from "next/router";
 
 const FilterContext = createContext({});
 type FilterContextType = {
@@ -14,6 +21,18 @@ export const useFilter = (): FilterContextType =>
 
 export const FilterProvider = ({ children }: { children: ReactNode }) => {
 	const [filters, setFilters] = useState<FilterValues>(DEFAULT_INITIALS);
+	const { query } = useRouter();
+
+	useEffect(() => {
+		if (query && Object.keys(query).length > 0) {
+			setFilters({
+				dateMin: (query.dateMin as unknown as Date) || DEFAULT_INITIALS.dateMin,
+				dateMax: (query.dateMax as unknown as Date) || DEFAULT_INITIALS.dateMax,
+				campus: (query.campus as string) || DEFAULT_INITIALS.campus,
+				promotion: (query.promotion as string) || DEFAULT_INITIALS.promotion,
+			} as FilterValues);
+		}
+	}, [query]);
 
 	return (
 		<FilterContext.Provider
