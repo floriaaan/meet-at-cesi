f5b7faa78e044b8e3ca69d90a77f869439e70b17
Florian LEROUX <10078837+floriaaan@users.noreply.github.com>
2023-05-12 00:40:08 +0200

    fix(email-verification): serverside related bug

--- lib/tokens/crypto-random-string.ts
+++ lib/tokens/crypto-random-string.ts
@@ -1,3 +1,7 @@
+import cryptoNode from "crypto";
 export const cryptoRandomString = (length: number): string => {
+	if (typeof window === "undefined" || !("crypto" in window)) {
+		return cryptoNode.createHash("sha256").digest("hex").slice(0, length);
+	}
 	return crypto.randomUUID().replace(/-/g, "").slice(0, length);
 };
