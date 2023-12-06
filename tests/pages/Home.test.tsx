e9a9c5798f4ae6c0d03e30a552d4401792fd016a
Florian LEROUX <florian.leroux3@laposte.net>
2023-12-06 15:14:47 +0100

    fix(tests): revert the home layout change in tests

--- tests/pages/Home.test.tsx
+++ tests/pages/Home.test.tsx
@@ -3,21 +3,18 @@ import { render, screen } from "@testing-library/react";
 
 import Home from "@/pages/index";
 import campusList from "@/resources/campus-list";
-import { event } from "@/tests/mocks/events";
 
 test("Home page", () => {
-	render(<Home event={event} />);
+	render(<Home caption={"Caption"} />);
 
+	// Check that the campus list is rendered
 	const campusListElement = screen.getByTestId("home-campus-list");
 	expect(campusListElement).toBeDefined();
 
-	// Check that the campus list is rendered
-	const categories = Array.from(
-		new Set(campusList.map((campus) => campus.category)),
-	);
-	expect(campusListElement.children.length).toBe(categories.length);
+	expect(campusListElement.children.length).toBe(campusList.length);
 
-	// Check that the EventListItem is rendered
-	const eventListItemElement = screen.getByTestId("home-event-list-item");
-	expect(eventListItemElement).toBeDefined();
+	// Check that the caption is rendered
+	const captionElement = screen.getByTestId("home-caption");
+	expect(captionElement).toBeDefined();
+	expect(captionElement.textContent).toBe("Caption");
 });
